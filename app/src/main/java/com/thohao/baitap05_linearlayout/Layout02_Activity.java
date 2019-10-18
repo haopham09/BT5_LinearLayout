09d9440e29d1d25d3548c56b4e6d408faca993ea
Hao Pham <phamvanhao009@gmail.com>
2019-10-18 17:12:00 +0700

    BT 5 :LinearLayout 2

---
+++ app/src/main/java/com/thohao/baitap05_linearlayout/Layout02_Activity.java
@@ -0,0 +1,14 @@
+package com.thohao.baitap05_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout02_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout02_);
+    }
+}
