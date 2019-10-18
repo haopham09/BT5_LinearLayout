8c99a3158df306539556a537cf9e34f1f2dbcae0
Hao Pham <phamvanhao009@gmail.com>
2019-10-18 17:50:43 +0700

    BT05: LinearLayout 3

---
+++ app/src/main/java/com/thohao/baitap05_linearlayout/Layout01_Activity.java
@@ -0,0 +1,14 @@
+package com.thohao.baitap05_linearlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Layout01_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout01_);
+    }
+}
