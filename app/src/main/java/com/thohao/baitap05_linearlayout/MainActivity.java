3d3713f953fbd5fba86d1b52c557dd2c0488ee14
Hao Pham <phamvanhao009@gmail.com>
2019-10-18 18:01:49 +0700

    Bai tap 05 : linear Finish

--- app/src/main/java/com/thohao/baitap05_linearlayout/MainActivity.java
+++ app/src/main/java/com/thohao/baitap05_linearlayout/MainActivity.java
@@ -2,13 +2,47 @@ package com.thohao.baitap05_linearlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn01, btn02, btn03;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Controller();
+
+    }
+    private void Controller(){
+        btn01=(Button)findViewById(R.id.btn_01);
+        btn01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Layout01_Activity.class);
+                startActivity(intent);
+            }
+        });
+        btn02=(Button)findViewById(R.id.btn_02);
+        btn02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Layout02_Activity.class);
+                startActivity(intent);
+            }
+        });
+        btn03=(Button)findViewById(R.id.btn_03);
+        btn03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(MainActivity.this, Layout03_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
